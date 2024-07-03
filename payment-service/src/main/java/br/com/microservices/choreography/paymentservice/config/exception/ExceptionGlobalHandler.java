7a142e0ed21fd373575efeebd1fca49eaff654f2
Jhonatas Coutinho <jhonatas.cjw@outlook.com>
2024-07-03 14:41:22 -0300

    Ajustes no Kafka e topicos

---
+++ payment-service/src/main/java/br/com/microservices/choreography/paymentservice/config/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,15 @@
+package br.com.microservices.choreography.paymentservice.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionGlobalHandler {
+
+    public ResponseEntity<?> handleValidationException(ValidationException validationException) {
+        var details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), validationException.getMessage());
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+    }
+
+}
